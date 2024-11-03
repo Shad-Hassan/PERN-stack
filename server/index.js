38325522a9308d48dda3c3968701897ce7ae470b
Shad Hassan <shadhassan8991@gmail.com>
2024-11-04 01:40:01 +0600

    json req.body working

--- server/index.js
+++ server/index.js
@@ -2,11 +2,29 @@ const express = require("express");
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require("cors");
- 
+const pool = require("./db");
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// routes
+// post items
+app.post("/items", async(req, res) => {
+  try {
+    console.log(req.body)
+  }
+  catch (err) {
+    console.error(err.message)
+  }
+})
+// fetch items
+
+// update items
+
+// delete items
+
+
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-  })
+  console.log(`Server is running on port ${port}`)
+})
